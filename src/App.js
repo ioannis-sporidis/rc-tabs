c36e985f5f18cab0640b3cf2d4229e6a7b3a0676
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-27 07:34:57 -0400

    Setup fetch api data and loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
+import { useState, useEffect } from 'react';
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
   return <div className='App'></div>;
 }
 
